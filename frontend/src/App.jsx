cc64758567b25615600ec8a482e69c4c55a044ee
Acash2018 <aakash7tripati@gmail.com>
2024-12-28 14:39:37 -0500

    We added the delete feature.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddFoodForm from './components/AddFoodForm';
 import FoodTable from "./components/FoodTable";
 import "./index.css";
-
 import axios from "./services/api";
 
 const App = () => {
   const [foodList, setFoodList] = useState([]);
 
+  // Fetch food items from the backend
   const fetchFoodItems = async () => {
     try {
-        const response = await axios.get("/food_items");
-        setFoodList(response.data.food_items); // Update the food list
-      } catch (error) {
-        console.error("Error fetching food items:", error);
-      }
+      const response = await axios.get("/food_items");
+      setFoodList(response.data.food_items); // Update the food list
+    } catch (error) {
+      console.error("Error fetching food items:", error);
+    }
   };
 
+  // Delete a food item
   const deleteFoodItem = async (id) => {
-    await axios.delete(`/delete_food/${id}`);
-    fetchFoodItems(); // Refresh the table after deletion
+    try {
+      await axios.delete(`/delete_food/${id}`);
+      fetchFoodItems(); // Refresh the table after deletion
+    } catch (error) {
+      console.error("Error deleting food item:", error);
+    }
   };
 
+  // Fetch the food items on component mount
   useEffect(() => {
     fetchFoodItems();
   }, []);
